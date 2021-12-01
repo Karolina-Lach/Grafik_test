67466e384defdcdc7386de7cdfb2728dfb82afd0
Karolina-Lach <carolina.lach@gmail.com>
2021-12-01 22:32:12 +0100

    Zmiana table na liste Dodanie słowanika z pensją pracowników

--- Grafik_test/ScheduleLogic/Schedule.cs
+++ Grafik_test/ScheduleLogic/Schedule.cs
@@ -8,21 +8,40 @@ namespace Grafik_test.ScheduleLogic
 {
     public class Schedule
     {
+        private readonly int _workersPerShift = 2;
         public struct Shift
         {
             public int FirstWorker { get; set; }
             public int SecondWorker { get; set; }
+
+            public void SetFirstWorker(int id)
+            {
+                FirstWorker = id;
+            }
+
+            public void SetSecondWorker(int id)
+            {
+                SecondWorker = id;
+            }
         }
 
-        public int ShiftsPerDay = 3;
-        readonly int minimumBreak = 11;
-        readonly int minimumWeekend = 35;
+        
+        readonly int _shiftsPerDay = 3;
+        readonly int _minimumBreak = 11;
+        readonly int _minimumWeekend = 35;
 
-        public float wagePerHour = 30;
-        public float wagePerHolidayHour = 60;
+        readonly float _wagePerHour = 30;
+        readonly float _wagePerHolidayHour = 60;
+
+        readonly float _salaryPerShiftHoliday;
+        readonly float _salaryPerShift;
+
+
         public Month Month;
         public int NumberOfAvailableWorkers;
         public int NumberOfShifts;
+
+        public Dictionary<int, float> SalaryPerWorker;
 
         public Shift[] ScheduleTable { get; set; }
 
@@ -44,21 +63,27 @@ namespace Grafik_test.ScheduleLogic
         {
             Month = new Month(month, year);
             NumberOfAvailableWorkers = workers.Count;
-            NumberOfShifts = Month.NumberOfDays * ShiftsPerDay;
+            NumberOfShifts = Month.NumberOfDays * _shiftsPerDay;
             ScheduleTable = new Shift[NumberOfShifts];
 
             Workers = new List<int>();
-            workers.ForEach(worker => Workers.Add(worker.Id));
+            foreach (Worker worker in workers)
+            {
+                Workers.Add(worker.Id);
+                SalaryPerWorker.Add(worker.Id, 0.0f);
+            }
 
             foreach (Wage wage in wages)
             {
                 if (wage.IsHoliday)
                 {
-                    wagePerHolidayHour = wage.WagePerHour;
+                    _wagePerHolidayHour = wage.WagePerHour;
+                    _salaryPerShiftHoliday = _wagePerHolidayHour * _shiftsPerDay;
                 }
                 else
                 {
-                    wagePerHour = wage.WagePerHour;
+                    _wagePerHour = wage.WagePerHour;
+                    _salaryPerShift = _wagePerHour * _shiftsPerDay;
                 }
             }
         }
@@ -67,50 +92,47 @@ namespace Grafik_test.ScheduleLogic
         public Schedule(int month, int year, List<Worker> workers, List<Wage> wages,
                         int numberOfShifts, int minBreak, int minWeekend) : this(month, year, workers, wages)
         {
-            ShiftsPerDay = numberOfShifts;
-            minimumBreak = minBreak;
-            minimumWeekend = minWeekend;
+            _shiftsPerDay = numberOfShifts;
+            _minimumBreak = minBreak;
+            _minimumWeekend = minWeekend;
         }
 
 
         public Shift[] CreateSchedule()
         {
-            Queue<int> availableWorkers = new Queue<int>();
-            foreach (var worker in Workers)
-            {
-                availableWorkers.Enqueue(worker);
-            }
-
-            for (int i = 0; i < NumberOfShifts; i++)
-            {
-                if (availableWorkers.Count == 0)
-                {
-                    /** Powinien być kod sprawdzający którzy pracownicy mogą pracować **/
-                    foreach (var worker in Workers)
-                    {
-                        availableWorkers.Enqueue(worker);
-                    }
-                }
-                ScheduleTable[i].FirstWorker = availableWorkers.Dequeue();
-
-                if (availableWorkers.Count == 0)
-                {
-                    /** Powinien być kod sprawdzający którzy pracownicy mogą pracować **/
-                    foreach (var worker in Workers)
-                    {
-                        availableWorkers.Enqueue(worker);
-                    }
-                }
-
-                ScheduleTable[i].SecondWorker = availableWorkers.Dequeue();
-            }
-
+            List<Shift> scheduleList = InitSchedule();
+            /*
+                Manipulowanie grafikiem tak żeby było lepiej...
+            */
             return ScheduleTable;
         }
 
+        private List<Shift> InitSchedule()
+        {
+            
+            int numberOfPositions = NumberOfShifts * _workersPerShift;
+            int listSize = (int)Math.Ceiling(Convert.ToDecimal(numberOfPositions / NumberOfAvailableWorkers)) * NumberOfAvailableWorkers;
+            List<Shift> scheduleList = new List<Shift>(listSize);
+            int j = 0;
+            for (int i=0;i<listSize;i++)
+            {
+                scheduleList[j].SetFirstWorker(j);
+
+                j++;
+                if (j >= NumberOfAvailableWorkers) j = 0;
+
+                scheduleList[j].SetSecondWorker(j);
+
+                j++;
+                if (j >= NumberOfAvailableWorkers) j = 0;
+            }
+
+            InitSalaryDictionary(scheduleList);
+            return scheduleList;
+        }
         private int GetDay(int shiftNumber)
         {
-            return (shiftNumber / ShiftsPerDay) + 1;
+            return (shiftNumber / _shiftsPerDay) + 1;
         }
         public bool IsDayHoliday(int day)
         {
@@ -120,44 +142,161 @@ namespace Grafik_test.ScheduleLogic
         {
             return IsDayHoliday(GetDay(shiftNumber));
         }
-        private int GetLastShift(int workerId, int currentShift)
+
+        private static int GetLastShift(List<Shift> schedule, int workerId, int currentShift)
         {
-            for (int i = 0; i < currentShift; i++)
-            {
-                if (ScheduleTable[i].FirstWorker == workerId || ScheduleTable[i].SecondWorker == workerId)
-                    return i;
-            }
-            return -1;
+            return schedule.FindLastIndex(currentShift, w => w.FirstWorker == workerId
+                                                                            || w.SecondWorker == workerId);
         }
-        private int GetBreakLength(int workerId, int currentShift)
+        private int GetBreakLength(List<Shift> schedule, int workerId, int currentShift)
         {
-            int lastShift = GetLastShift(workerId, currentShift);
+            int lastShift = GetLastShift(schedule, workerId, currentShift);
             if (lastShift == -1)
             {
-                return minimumBreak;
+                return _minimumBreak;
             }
 
-            return (currentShift - lastShift) * 8;
+            return (currentShift - lastShift) * (24 / _shiftsPerDay);
         }
-        private float GetCurrentSalary(int workerId, int currentShift)
+        private void InitSalaryDictionary(List<Shift> schedule)
         {
-            float salary = 0;
-            for (int i = 0; i < currentShift; i++)
+            // Liczymy tylko tyle ile jest zmian, nie dla całej listy "schedule"
+            // W liście schedule może być więcej pozycji niż rzeczywiście jest zmian
+            // w miesiącu (16 * 12)
+            for (int i=0; i < NumberOfShifts; i++)
             {
-                if (ScheduleTable[i].FirstWorker == workerId || ScheduleTable[i].SecondWorker == workerId)
+                if (IsShiftHoliday(i))
                 {
-                    if (IsShiftHoliday(i))
-                    {
-                        salary += wagePerHolidayHour * (24 / ShiftsPerDay);
-                    }
-                    else
-                    {
-                        salary += wagePerHour * (24 / ShiftsPerDay);
-                    }
+                    SalaryPerWorker[schedule[i].FirstWorker] += _salaryPerShiftHoliday;
+                    SalaryPerWorker[schedule[i].SecondWorker] += _salaryPerShiftHoliday;
+                }
+                else
+                {
+                    SalaryPerWorker[schedule[i].FirstWorker] += _salaryPerShift;
+                    SalaryPerWorker[schedule[i].SecondWorker] += _salaryPerShift;
                 }
             }
-
-            return salary;
         }
+        private void AddToSalary(int workerId, int newShiftNumber)
+        {
+            if(newShiftNumber < NumberOfShifts)
+            {
+                if(IsShiftHoliday(newShiftNumber))
+                {
+                    SalaryPerWorker[newShiftNumber] += _salaryPerShiftHoliday;
+                }
+                else
+                {
+                    SalaryPerWorker[newShiftNumber] += _salaryPerShiftHoliday;
+                }
+            }
+        }
+        private void RemoveFromSalary(int workerId, int prevShiftNumber)
+        {
+            if (prevShiftNumber < NumberOfShifts)
+            {
+                if (IsShiftHoliday(prevShiftNumber))
+                {
+                    SalaryPerWorker[prevShiftNumber] -= _salaryPerShiftHoliday;
+                }
+                else
+                {
+                    SalaryPerWorker[prevShiftNumber] -= _salaryPerShiftHoliday;
+                }
+            }
+        }
+        
+        private int WorkerWithMaxSalary()
+        {
+            var richestWorkers = SalaryPerWorker.Where(x => x.Value == SalaryPerWorker.Max(v => v.Value));
+            return richestWorkers.FirstOrDefault().Key;
+        }
+
+        private int WorkerWithMinSalary()
+        {
+            var richestWorkers = SalaryPerWorker.Where(x => x.Value == SalaryPerWorker.Min(v => v.Value));
+            return richestWorkers.FirstOrDefault().Key;
+        }
+
+        private float GetDifferenceMinMaxSalary()
+        {
+            return SalaryPerWorker[WorkerWithMaxSalary()] - SalaryPerWorker[WorkerWithMinSalary()];
+        }
+        /******************* LEGACY ******************/
+        //private int GetLastShift(int workerId, int currentShift)
+        //{
+        //    for (int i = 0; i < currentShift; i++)
+        //    {
+        //        if (ScheduleTable[i].FirstWorker == workerId || ScheduleTable[i].SecondWorker == workerId)
+        //            return i;
+        //    }
+        //    return -1;
+        //}
+        //private int GetBreakLength(int workerId, int currentShift)
+        //{
+        //    int lastShift = GetLastShift(workerId, currentShift);
+        //    if (lastShift == -1)
+        //    {
+        //        return minimumBreak;
+        //    }
+
+        //    return (currentShift - lastShift) * 8;
+        //}
+        //private float GetCurrentSalary(int workerId, int currentShift)
+        //{
+        //    float salary = 0;
+        //    for (int i = 0; i < currentShift; i++)
+        //    {
+        //        if (ScheduleTable[i].FirstWorker == workerId || ScheduleTable[i].SecondWorker == workerId)
+        //        {
+        //            if (IsShiftHoliday(i))
+        //            {
+        //                salary += wagePerHolidayHour * (24 / ShiftsPerDay);
+        //            }
+        //            else
+        //            {
+        //                salary += wagePerHour * (24 / ShiftsPerDay);
+        //            }
+        //        }
+        //    }
+
+        //    return salary;
+        //}
+
+
+        //public Shift[] CreateSchedule()
+        //{
+        //    Queue<int> availableWorkers = new Queue<int>();
+        //    foreach (var worker in Workers)
+        //    {
+        //        availableWorkers.Enqueue(worker);
+        //    }
+
+        //    for (int i = 0; i < NumberOfShifts; i++)
+        //    {
+        //        if (availableWorkers.Count == 0)
+        //        {
+        //            /** Powinien być kod sprawdzający którzy pracownicy mogą pracować **/
+        //            foreach (var worker in Workers)
+        //            {
+        //                availableWorkers.Enqueue(worker);
+        //            }
+        //        }
+        //        ScheduleTable[i].FirstWorker = availableWorkers.Dequeue();
+
+        //        if (availableWorkers.Count == 0)
+        //        {
+        //            /** Powinien być kod sprawdzający którzy pracownicy mogą pracować **/
+        //            foreach (var worker in Workers)
+        //            {
+        //                availableWorkers.Enqueue(worker);
+        //            }
+        //        }
+
+        //        ScheduleTable[i].SecondWorker = availableWorkers.Dequeue();
+        //    }
+
+        //    return ScheduleTable;
+        //}
     }
 }
