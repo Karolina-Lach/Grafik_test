1b5fe05d521e90ee57366b19907bb712fe352dc4
Karolina-Lach <carolina.lach@gmail.com>
2022-01-17 09:34:11 +0100

    Zmiany layout + kryterium weekendu

---
+++ Grafik_test/Models/ViewModels/ScheduleOneWorkerVM.cs
@@ -0,0 +1,20 @@
+﻿using Grafik_test.ScheduleLogic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Grafik_test.Models.ViewModels
+{
+    public class ScheduleOneWorkerVM
+    {
+        public Schedule Schedule;
+        public Worker Worker;
+
+    
+        [System.Obsolete]
+        public ScheduleOneWorkerVM(Schedule schedule, Worker worker)
+        {
+            Worker = worker;
+            Schedule = schedule;
+        }
+    }
+}
